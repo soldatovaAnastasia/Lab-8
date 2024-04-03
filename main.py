2344029717892638cedb3995cb555f882744f8e6
Soldatova Anastasia <anastasiasoldatova927@gmail.com>
2024-04-03 19:28:15 +0300

    end

--- main.py
+++ main.py
@@ -1,69 +1,58 @@
-import time
+import numpy as np
+import cv2 
 
-import cv2
+def add_noise():  # Определяем вместе с пользователем коэффициент шума
+    noise_multiplier = int(input('Введите коэффициент для желаемого шума (1-99): '))
+    if noise_multiplier > 99: 
+        noise_multiplier = 0.99  
+    elif noise_multiplier < 1: 
+        noise_multiplier = 0.01 
+    else: 
+        noise_multiplier /= 100 
 
+    img = cv2.imread(r'variant-5.jpg', cv2.IMREAD_COLOR)
+    image_arr = np.array(img/255, dtype=float)
+    noise_arr = np.random.normal(0, noise_multiplier, img.shape)  # Генерируем случайный шум
 
-def image_processing():
-    img = cv2.imread('img_test.jpg')
-    #cv2.imshow('image', img)
-    w, h = img.shape[:2]
-    #(cX, cY) = (w // 2, h // 2)
-    #M = cv2.getRotationMatrix2D((cX, cY), 45, 1.0)
-    #rotated = cv2.warpAffine(img, M, (w, h))
-    #cv2.imshow('rotated', rotated)
+    image_final = image_arr + noise_arr  # Добавляем сгенерированный шум к изображению
 
-    #cat = img[250:580, 20:280]
-    #cv2.imshow('image', cat)
+    cv2.imshow(f'Noise {noise_multiplier}', image_final)  # Изображение с добавленным шумом
+    cv2.waitKey(0)
 
-    #r = cv2.selectROI(img)
-    #image_cropped = img[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
-    #cv2.imshow('cropped', image_cropped)
+#Модификация кода из п2
+def circle_tracking():  
+    video = cv2.VideoCapture(r'sample.mp4')
+    detector = cv2.createBackgroundSubtractorMOG2(history=100)
 
-    cv2.line(img, (0, 0), (580, 600), (255, 0, 0), 5)
-    cv2.rectangle(img, (384, 10), (580, 128), (0, 252, 0), 3)
-    cv2.putText(img, 'Lab. No 8', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 3,
-                (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.imshow('img', img)
-
-
-def video_processing():
-    cap = cv2.VideoCapture(1)
-    down_points = (640, 480)
-    i = 0
+    # Ищем контуры
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+        rat, frame = video.read() 
+        mask = detector.apply(frame)
+        _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+        object_contour, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        frame = cv2.resize(frame, down_points, interpolation=cv2.INTER_LINEAR)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        ret, thresh = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY_INV)
+        #Проверяем контур и рисуем круги
+        for countur in object_contour: 
+            area = cv2.contourArea(countur) 
+            if area > 450: 
+                (x, y), r = cv2.minEnclosingCircle(countur) 
+                center = (int(x), int(y))  
+                r = int(r) 
+                if center[0] < 250 and center[1] < 210: 
+                    cv2.circle(frame, center, r, (255, 0, 0), 3) 
+                elif center[0] > 340 and center[1] > 300:  
+                    cv2.circle(frame, center, r, (0, 0, 255), 3) 
+                else: 
+                    cv2.circle(frame, center, r, (0, 0, 0), 3) 
+         
+        # Отображаем кадр с обнаруженными кругами
+        cv2.imshow('Frame', frame)
+        key = cv2.waitKey(30)  
+        if key == 27:  
+            break  
 
-        contours, hierarchy = cv2.findContours(thresh,
-                            cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        if len(contours) > 0:
-            c = max(contours, key=cv2.contourArea)
-            x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            if i % 5 == 0:
-                a = x + (w // 2)
-                b = y + (h // 2)
-                print(a, b)
+    video.release() 
+    cv2.destroyAllWindows() 
 
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-        time.sleep(0.1)
-        i += 1
-
-    cap.release()
-
-
-if __name__ == '__main__':
-    #image_processing()
-    video_processing()
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+add_noise() 
+circle_tracking()
